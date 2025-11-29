d7a1019115b6b1d02626e7d0cf19a00c4c76feda
SATHISH B <sathishb2310@gmail.com>
2025-11-29 23:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//your JS code here. If required.
-// your JS code here. If required.
-
 const output = document.getElementById("output");
 
 // Given array
@@ -13,29 +10,27 @@ function initialPromise() {
     });
 }
 
- // First transformation: filter out odd numbers
-        function filterOddNumbers(numbers) {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    const evens = numbers.filter(num => num % 2 === 0);
-                    output.innerText = `After filtering odd numbers: [${evens.join(", ")}]`;
-                    resolve(evens);
-                }, 1000)
+// First transformation: filter out odd numbers
+function filterOddNumbers(numbers) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const evens = numbers.filter(num => num % 2 === 0);
+            output.innerText = `After filtering odd numbers: [${evens.join(", ")}]`;
+            resolve(evens);
+        }, 1000);
+    });
+}
 
-            });
-        }
-
-        // Second transformation: multiply even numbers by 2
-        function multiplyByTwo(numbers) {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    const multiplied = numbers.map(num => num * 2);
-                    output.innerText += `\nAfter multiplying by 2: [${multiplied.join(", ")}]`;
-                    resolve(multiplied);
-                }, 2000);
-            });
-        }
-
+// Second transformation: multiply even numbers by 2
+function multiplyByTwo(numbers) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const multiplied = numbers.map(num => num * 2);
+            output.innerText += `\nAfter multiplying by 2: [${multiplied.join(", ")}]`;
+            resolve(multiplied);
+        }, 2000);
+    });
+}
 
 // Chaining the promises
 initialPromise()
