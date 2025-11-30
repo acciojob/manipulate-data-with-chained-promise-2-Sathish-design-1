fc1b453de105417c58ba240c00f2af7588cfd485
SATHISH B <sathishb2310@gmail.com>
2025-11-30 22:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-const output = document.getElementById("output");
+// script.js
 
-// Given array
-const arr = [1, 2, 3, 4];
-
-// Initial promise: resolves after 3 seconds
-function initialPromise() {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(arr), 3000);
-    });
+function wait(ms, value) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(value), ms);
+  });
 }
 
-// First transformation: filter out odd numbers
-function filterOddNumbers(numbers) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            const evens = numbers.filter(num => num % 2 === 0);
-            output.innerText = `After filtering odd numbers: [${evens.join(", ")}]`;
-            resolve(evens);
-        }, 1000);
-    });
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const output = document.getElementById("output");
 
-// Second transformation: multiply even numbers by 2
-function multiplyByTwo(numbers) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            const multiplied = numbers.map(num => num * 2);
-            output.innerText += `\nAfter multiplying by 2: [${multiplied.join(", ")}]`;
-            resolve(multiplied);
-        }, 2000);
-    });
-}
+  const arr = [1, 2, 3, 4];  // Given array
 
-// Chaining the promises
-initialPromise()
-    .then(filterOddNumbers)
-    .then(multiplyByTwo)
-    .catch(err => {
-        output.innerText = "Error: " + err;
+  wait(3000, arr)
+    .then((numbers) => {
+
+      return wait(1000, numbers.filter(num => num % 2 === 0));
+    })
+    .then((evenNumbers) => {
+      output.innerText = evenNumbers.join(", ");   // Display [2, 4]
+
+      return wait(2000, evenNumbers.map(num => num * 2));
+    })
+    .then((finalArray) => {
+      output.innerText = finalArray.join(", ");    // Display [4, 8]
+    })
+    .catch((err) => {
+      output.innerText = "Error: " + err;
     });
+});
